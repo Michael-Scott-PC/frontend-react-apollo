1342e8ecb7a1085ab291d2b9b4d12aa5b819da70
Michael-Scott-PC <43099976+Michael-Scott-PC@users.noreply.github.com>
2022-05-02 16:48:59 -0400

    Initialize ApolloClient, add prettier, change graphql version

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { createRoot } from 'react-dom/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
-import App from "./App";
+import App from './App';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
+const client = new ApolloClient({
+  uri: 'https://3zgymj.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
+
 root.render(
-  <StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </StrictMode>
+  </ApolloProvider>
 );
