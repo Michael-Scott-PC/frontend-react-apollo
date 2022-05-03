334ff5b0276e154d4e102df0e1632a7e09b3ec1c
Michael-Scott-PC <43099976+Michael-Scott-PC@users.noreply.github.com>
2022-05-02 22:23:34 -0400

    Some buzzer beaters Tried to get the dropdown nav up and functioning and as much styling as possible, but didn't get very far. Forgot to note in the Footer I used p tags since nothing's linking any where, I probably still should have used a tags with some placeholder url. Thank you.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import Button from './Button';
 
 const NavBar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <div className="flex justify-between pt-5 px-5">
       <img src="images/rebrandly-logo.png" alt="rebrandly logo" />
-      <GiHamburgerMenu class="lg:hidden" />
-      <nav class="hidden lg:flex justify-between w-10/12">
+      <GiHamburgerMenu class="lg:hidden" onClick={() => setToggle(!toggle)} />
+      <nav
+        class={`${
+          toggle ? 'flex' : 'hidden'
+        } absolute bg-white w-full h-3/5 flex-col text-center lg:static lg:flex lg:flex-row justify-between w-10/12`}
+      >
         <a href="https://localhost:3000/">Features</a>
         <a href="https://localhost:3000/">Domains</a>
         <a href="https://localhost:3000/">Pricing</a>
