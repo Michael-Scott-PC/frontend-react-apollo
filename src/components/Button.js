f0ea16ce33ac9fc7231241571ceaad8be6afb01a
Michael-Scott-PC <43099976+Michael-Scott-PC@users.noreply.github.com>
2022-05-02 22:11:45 -0400

    Add NavBar Links Added a LG breakpoint to hide the hamburger icon and display the links in the navbar Updated the Button component to accept a border property

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 const Button = ({
   bgColor = 'bg-white',
+  border = 'none',
   text = 'default text',
   textColor = 'text-sky-600',
   width = 'w-2/3'
 }) => {
   return (
     <div>
-      <button className={`${bgColor} ${textColor} ${width} p-1 mb-1 rounded`}>
+      <button
+        className={`${bgColor} ${border} ${textColor} ${width} p-1 mb-1 rounded`}
+      >
         {text}
       </button>
     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
+import Button from './Button';
 
 const NavBar = () => {
   return (
     <div className="flex justify-between pt-5 px-5">
       <img src="images/rebrandly-logo.png" alt="rebrandly logo" />
-      <GiHamburgerMenu />
+      <GiHamburgerMenu class="lg:hidden" />
+      <nav class="hidden lg:flex justify-between w-10/12">
+        <a href="https://localhost:3000/">Features</a>
+        <a href="https://localhost:3000/">Domains</a>
+        <a href="https://localhost:3000/">Pricing</a>
+        <a href="https://localhost:3000/">Enterprise</a>
+        <a href="https://localhost:3000/">Login</a>
+        <a href="https://localhost:3000/">Sign Up</a>
+        <Button
+          border="border border-sky-600"
+          text="Get a quote"
+          width="w-full"
+        />
+      </nav>
     </div>
   );
 };
